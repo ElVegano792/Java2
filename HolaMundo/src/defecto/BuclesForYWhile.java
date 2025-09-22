4eb0f0917157f0dfaab777c657441f59802bb25d
alumno <alumno@1DAM-05>
2025-09-22 12:22:12 +0200

    añadir ej de bucles con for y while

---
+++ HolaMundo/src/defecto/BuclesForYWhile.java
@@ -0,0 +1,47 @@
+package defecto;
+
+import java.util.Scanner;
+
+
+
+public class BuclesForYWhile {
+
+	public static void main(String[] args) {
+	
+	/*for(int i=0; i<5; i++)
+		System.out.println(i); */
+	
+	/*int i=0;
+	while(i<5) {
+		System.out.println(i);
+		i++;
+	} */
+	
+	
+	Scanner teclado = new Scanner(System.in);
+	/*int numero;
+	System.out.println("Escriba un numero: ");
+	numero = teclado.nextInt();
+	int i = numero;
+	while(i==0) {
+		System.out.println(numero);
+		i++;
+	}*/
+
+	int tecla = 9;
+	while(tecla !=0) {
+		System.out.println("Escribe un entero. 0 para salir: ");
+		tecla = teclado.nextInt();
+		System.out.println(tecla);
+	}
+		
+		
+	// Que no muestre los numeros que metemos por teclado hasta cuando el usuario meta el numero 0
+	
+	
+	
+	
+	
+	
+	}
+}
